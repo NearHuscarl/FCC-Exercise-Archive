fc60edf2e7193bec9e089eb550ef189a1cd084f9
Near <16520846@gm.uit.edu.vn>
2017-06-05 00:41:15 +0700

    delete and only keep related files

--- 4. Basic Font End Development Project/Tribute Page/Reference Tribute Page/Star Wars/Star Wars.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".img-hider").click(function() {
-  $(".thumbnail").toggle( "slow", function() {
-    // Animation complete.
-  })
-  });
-});
-
-//still needs work
